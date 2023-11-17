dc2342f9ba0f907311a330343dfc67dc5837277c
Jeff <lijeff30@gmail.com>
2023-11-17 13:53:54 -0500

    Added react-bootstrap and test button

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./LandingPage.css";
 import logo from "../assets/SOCSLogo.png";
 import ParticlesBackground from "./ParticlesBackground"
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function LandingPage() {
     return(
@@ -10,6 +12,7 @@ function LandingPage() {
             <a href={"#0"}> 
                 <img src={logo} style={{ width: '300px', height: 'auto' }} className="SOCSlogo" alt="logo" />
             </a>
+            <Button variant="primary">Primary</Button>{' '}
         </header>
 
         <div className="MiddleContent">

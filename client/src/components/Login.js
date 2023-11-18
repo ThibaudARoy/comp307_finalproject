ac0e4495f0aec062edf285349efc0256165d6b60
Jeff <lijeff30@gmail.com>
2023-11-18 00:02:58 -0500

    Added Login content and CSS

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,26 @@
+import "./Login.css";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ParticlesBackground from "./ParticlesBackground";
+
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <div className="back">
+            <div className="LoginPage">
+                Logo
+                <h1>Sign in to SOCS Boards</h1>
+                <p>We suggest using your McGill email.</p>
+                <div className="inputs">
+                    <h5>Email <span className="required">*</span></h5>
+                    <input type = "text"></input>
+                    <p></p>
+                    <h5>Password <span className="required">*</span></h5>
+                    <input type = "password"></input>
+                </div>
+                
+                <Button variant="danger" className="login-button" size="lg">Login</Button>{' '}
+            </div>
+        </div>  
     )
 }
 

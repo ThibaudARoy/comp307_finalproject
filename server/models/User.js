4b6d3ffea58dd7c385385758d8b7abafc143a70b
catrionamcintosh01 <catriona.mcintosh@mail.mcgill.ca>
2023-11-18 16:43:27 -0500

    adding register and signin endpoints

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs")
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -7,5 +8,34 @@ const UserSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   boards: [{ type: mongoose.Schema.Types.ObjectId, ref: "Board" }],
 });
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (this.isModified('password') || this.isNew) {
+      bcrypt.genSalt(10, function (err, salt) {
+          if (err) {
+              return next(err);
+          }
+          bcrypt.hash(user.password, salt, null, function (err, hash) {
+              if (err) {
+                  return next(err);
+              }
+              user.password = hash;
+              next();
+          });
+      });
+  } else {
+      return next();
+  }
+});
+
+UserSchema.methods.comparePassword = function (passw, cb) {
+  bcrypt.compare(passw, this.password, function (err, isMatch) {
+      if (err) {
+          return cb(err);
+      }
+      cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);

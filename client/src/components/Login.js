edea6f09603eb25c66a1a77d7d471466a73efacc
Jeff <lijeff30@gmail.com>
2023-11-19 21:18:11 -0500

    Added Logo on Login Page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,12 +2,14 @@ import "./Login.css";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from "react-router-dom";
+import logo from "../assets/bird.png";
 
 function Login() {
     return (
         <div className="back">
             <div className="LoginPage">
-                Logo
+                <img src={logo} style={{ width: '70px', height: '70px' }} alt="logo" />
+                <p></p>
                 <h1>Sign in to SOCS Boards</h1>
                 <p>We suggest using your McGill email.</p>
                 <div className="inputs">

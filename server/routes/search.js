ee0ca7a7d695c765bb91aa84cf4c55b0f9584b15
ThibaudARoy <thibone14@gmail.com>
2023-12-11 09:56:25 -0500

    Formatting for Search

--- server/routes/search.js
+++ server/routes/search.js
@@ -13,29 +13,22 @@ router.get("/search/:boardId", async (req, res) => {
     `Received search request - Board ID: ${boardId}, Search Term: '${searchTerm}'`
   );
 
-  // Validate search term
   if (!searchTerm) {
-    console.log("Search term is missing");
     return res.status(400).send("Search term is required");
   }
 
   try {
-    // Fetch channels associated with the board
     console.log(`Fetching channels for board ID: ${boardId}`);
     const channels = await Channel.find({ board: boardId });
 
     // Handling no channels found
     if (channels.length === 0) {
-      console.log(`No channels found for board ID: ${boardId}`);
       return res.status(404).send(`No channels found for board: ${boardId}`);
     }
 
-    // Logging found channel IDs
     const channelIds = channels.map((channel) => channel._id);
-    console.log(`Found channels - IDs: ${channelIds.join(", ")}`);
 
     // Perform the search in messages
-    console.log(`Searching messages in channels for term: '${searchTerm}'`);
     const messages = await mongoose.connection
       .collection("messages")
       .aggregate([
@@ -47,30 +40,40 @@ router.get("/search/:boardId", async (req, res) => {
         },
         {
           $lookup: {
-            from: "users", // The collection to join
-            localField: "creator", // Field from the input documents
-            foreignField: "_id", // Field from the documents of the "from" collection
-            as: "creatorDetails", // Output array field
+            from: "users",
+            localField: "creator",
+            foreignField: "_id",
+            as: "creatorDetails",
           },
         },
         {
-          $unwind: "$creatorDetails", // Flatten the output
+          $unwind: "$creatorDetails",
+        },
+        {
+          $addFields: {
+            formattedTimestamp: {
+              $dateToString: {
+                format: "%b %d",
+                date: "$timestamp",
+                timezone: "UTC",
+              },
+            },
+          },
         },
         {
           $project: {
-            _id: 1, // Include the message id
-            content: 1, // Include the message content
-            creatorDetails: { firstName: 1, lastName: 1 }, // Include specific fields from creatorDetails
+            _id: 1,
+            content: 1,
+            timestamp: 1,
+            formattedTimestamp: 1,
+            channel: 1,
+            creatorDetails: { firstName: 1, lastName: 1 },
           },
         },
       ])
       .toArray();
 
-    //.populate("creator", "firstName lastName");
-
-    // Handling no messages found
     if (messages.length === 0) {
-      console.log("No messages found matching the search term");
       return res.status(404).send("No messages found matching your search");
     }
 
@@ -78,7 +81,6 @@ router.get("/search/:boardId", async (req, res) => {
     console.log(`Found ${messages.length} message(s) matching the search term`);
     res.json(messages);
   } catch (error) {
-    // Detailed error logging
     console.error("Error in /search/:boardId endpoint:", error);
     res.status(500).send(`Server error: ${error.message}`);
   }
